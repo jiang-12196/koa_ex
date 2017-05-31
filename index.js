db10f7737c2a11bbadcdf49d1abf6558c3e00371
jiang1991 <jiang12196@126.com>
2017-06-01 07:36:02 +0800

    body html

--- index.js
+++ index.js
@@ -1,10 +1,48 @@
 const Koa = require('koa');
+const fs = require('fs');
 const app = new Koa();
+
+function render(page) {
+    return new Promise((resolve, reject) => {
+        let viewUrl = `./view/${page}`
+        fs.readFile(viewUrl, "binary", (err, data) => {
+            if(err) {
+                reject(err)
+            } else {
+                resolve(data);
+            }
+        })
+    })
+}
+
+async function route(url) {
+    let view = '404.html';
+    switch (url) {
+        case '/':
+            view = 'index.html';
+            break;
+        case '/index':
+            view = 'index.html';
+            break;
+        case '/todo':
+            view = 'todo.html';
+            break;
+        case '/404':
+            view = '404.html';
+            break;
+        default:
+            break;
+    }
+    let html = await render(view);
+    return html;
+}
 
 app.use( async (ctx) => {
     let url = ctx.request.url;
-    ctx.body = `<h1>hello koa</h1><p>${url}</p>`;
+    let html = await route(url);
+    ctx.body = html;
 });
+
 
 function getSyncTime() {
     return new Promise((resolve, reject) => {
@@ -32,6 +70,7 @@ async function getData() {
     console.log(data)
 }
 
-getData();
 
-app.listen(3000);
+app.listen(3000, ()=> {
+    console.log('the server is starting at port 3000');
+});
